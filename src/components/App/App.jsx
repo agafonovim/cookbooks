13e91c0f5562416821bcaf4c1e75003b1ce33536
Andrey Agafonov <agafonovim@AndyBook.local>
2019-11-26 11:58:23 +0300

    Добавил страницу 404

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Route,
+    Switch
+} from "react-router-dom";
+
 import './App.scss';
+import Sidebar from "../Sidebar/Sidebar";
+import Content from "../Content/Content";
+import NotFound from "../NotFound/NotFound";
 
 const App = () => {
     return (
-        <div className="app">
-        </div>
+        <Router>
+            <div className="app">
+                <Sidebar />
+                <Switch>
+                    <Route path="/browse" component={Content} />
+                    <Route path="*" component={NotFound} />
+                </Switch>
+            </div>
+        </Router>
     )
 };
 
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NotFound.scss';
+
+const NotFound = () => (
+    <div className="page page__content-center">
+        <div className="page__not-found"></div>
+    </div>
+);
+
+export default NotFound;
