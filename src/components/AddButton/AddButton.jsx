3a229d8a95adf60b836108e6a140c8650c26c54a
Andrey Agafonov <agafonovim@AndyBook.local>
2019-12-10 12:48:40 +0300

    Добавил атрибут type

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -4,7 +4,7 @@ import './AddButton.scss';
 const AddButton = props => {
     return (
         <div className="add-button__layer">
-            <button href="#" className="add-button" onClick={props.onClick}><i className="fa fa-plus" aria-hidden="true"></i>ADD A BOOK</button>
+            <button type="button" className="add-button" onClick={props.onClick}><i className="fa fa-plus" aria-hidden="true"></i>ADD A BOOK</button>
         </div>
     )
 };
