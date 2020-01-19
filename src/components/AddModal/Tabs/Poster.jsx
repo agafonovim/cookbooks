2fbc8bc4fc0cf3d80c6d92d8e3a515e1a8aa11b0
Andrey Agafonov <agafonovim@AndyBook.local>
2020-01-19 15:14:44 +0300

    Poster image validation

--- src/components/AddModal/Tabs/Poster.jsx
+++ src/components/AddModal/Tabs/Poster.jsx
@@ -5,24 +5,35 @@ import { useFormikContext } from 'formik';
 const Poster = props => {
     const formik = useFormikContext();
 
-    const handlePosterImage = (e) => {
-        let input = e.target;
-        // if (!formik.values.email) {
-        //     errors.email = 'Required';
-        // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        //     errors.email = 'Invalid email address';
-        // }
-        let reader = new FileReader();
+    const SUPPORTED_FORMATS = [
+        "image/jpg",
+        "image/jpeg",
+        "image/gif",
+        "image/png",
+        "image/svg+xml"
+    ];
 
-        reader.readAsDataURL(input.files[0]);
-        reader.onload = () => {
-            formik.setFieldValue("poster_image", reader.result);
+    const checkFormat = value => value && SUPPORTED_FORMATS.includes(value.type);
+
+    const handlePosterImage = (e) => {
+        let input = e.target.files[0];
+
+        if (checkFormat(input)) {
+            let reader = new FileReader();
+
+            reader.readAsDataURL(input);
+            reader.onload = () => {
+                formik.setFieldValue("poster_image", reader.result);
+            }
+        } else {
+            formik.setErrors({poster: "Unsupported format"});
         }
     }
 
     return (
         <>
             <Input title="Poster" type="file" accept="image/jpeg,image/png,image/gif,image/svg+xml" onChange={(e) => handlePosterImage(e)}>
+                {formik.errors.poster && formik.touched.poster && formik.errors.poster}
             </Input>
             {formik.values.poster_image && <img src={formik.values.poster_image} alt="preview_image" height={250} />}
         </>
